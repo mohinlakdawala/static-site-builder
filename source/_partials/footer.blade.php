1fc5b93cca7a0e8ae3fe67b785937972050dae8b
Mohin Lakdawala <mlakdawala@aecordigital.com>
2016-03-18 20:25:03 +0530

    Initial commit

---
+++ source/_partials/footer.blade.php
@@ -0,0 +1,6 @@
+<div class="container-fluid">
+    <hr>
+    <footer>
+        <p>&copy; 2015 Company, Inc.</p>
+    </footer>
+</div>
---
+++ source/about-us.blade.php
@@ -0,0 +1,40 @@
+@extends('_layouts.master')
+
+@section('plugin-styles')
+<link rel="stylesheet" href="/css/app.css">
+@endsection
+
+@section('body')
+<div class="container-fluid">
+    <section class="about-us">
+        <div class="col-md-8 col-md-offset-2 text-center">
+            <h1>About us</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, deserunt sint. Iste ipsum fugiat, earum reiciendis ducimus error maxime aperiam necessitatibus quos nisi aut libero magnam, unde velit perferendis debitis adipisci obcaecati quis quo alias placeat incidunt aspernatur. Quae explicabo tempore, at quam totam repellat ad autem delectus minus veniam iste, laudantium nisi exercitationem harum voluptas reprehenderit debitis! Eaque eveniet molestias velit, tempore iusto impedit illo minima ratione eius, beatae.</p>
+        </div>
+        <div class="col-md-3 text-center">
+            <h2><i class="fa fa-star"></i></h2>
+            <h2>Ipsum dolor</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit laborum, impedit sequi nihil minima aliquid saepe cupiditate quos quibusdam unde!</p>
+        </div>        
+        <div class="col-md-3 text-center">
+            <h2><i class="fa fa-flash"></i></h2>
+            <h2>Ipsum dolor</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit laborum, impedit sequi nihil minima aliquid saepe cupiditate quos quibusdam unde!</p>
+        </div>        
+        <div class="col-md-3 text-center">
+            <h2><i class="fa fa-building"></i></h2>
+            <h2>Ipsum dolor</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit laborum, impedit sequi nihil minima aliquid saepe cupiditate quos quibusdam unde!</p>
+        </div>        
+        <div class="col-md-3 text-center">
+            <h2><i class="fa fa-car"></i></h2>
+            <h2>Ipsum dolor</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit laborum, impedit sequi nihil minima aliquid saepe cupiditate quos quibusdam unde!</p>
+        </div>        
+    </section>
+</div>
+@endsection
+
+@section('plugin-scripts')
+
+@endsection
